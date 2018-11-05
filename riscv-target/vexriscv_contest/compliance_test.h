6f660cf74790a905347a2ebfe0f95c1e2a5433dc
Dolu1990 <charles.papon.90@gmail.com>
2018-11-05 12:14:47 +0100

    Add up5kPerf compliance

---
+++ riscv-target/vexriscv_contest/compliance_test.h
@@ -0,0 +1,110 @@
+// RISC-V Compliance Test Header File
+// Copyright (c) 2017, Codasip Ltd. All Rights Reserved.
+// See LICENSE for license details.
+//
+// Description: Common header file for RV32I tests
+
+#ifndef _COMPLIANCE_TEST_H
+#define _COMPLIANCE_TEST_H
+
+#include "riscv_test.h"
+
+//-----------------------------------------------------------------------
+// RV Compliance Macros
+//-----------------------------------------------------------------------
+
+#define RV_COMPLIANCE_HALT                                                    \
+        la a0, codasip_signature_start;                                        \
+        la a1, codasip_signature_end; \
+        li a2, 0xF0000; \
+complience_halt_loop: \
+        beq a0, a1, complience_halt_break; \
+        addi a3, a0, 16; \
+complience_halt_loop2: \
+        addi a3, a3, -1; \
+complience_halt_wait_uart: \
+        lw a4, 0 (a2); \
+        bne x0, a4, complience_halt_wait_uart;\
+        lb a4, 0 (a3); \
+        srai a5, a4, 4; \
+        andi a5, a5, 0xF; \
+        li a6, 10; \
+        blt a5, a6, notLetter; \
+        addi a5, a5, 39; \
+notLetter: \
+        addi a5, a5, 0x30; \
+        sw a5, 0 (a2); \
+complience_halt_wait_uart2: \
+        lw t0, 0 (a2); \
+        bne x0, t0, complience_halt_wait_uart2;\
+        srai a5, a4, 0; \
+        andi a5, a5, 0xF; \
+        li a6, 10; \
+        blt a5, a6, notLetter2; \
+        addi a5, a5, 39; \
+notLetter2: \
+        addi a5, a5, 0x30; \
+        sw a5, 0 (a2); \
+        bne a0, a3,complience_halt_loop2;  \
+        addi a0, a0, 16; \
+complience_halt_wait_uart3: \
+        lw a4, 0 (a2); \
+        bne x0, a4, complience_halt_wait_uart3;\
+        li a4, 0x0A; \
+        sw a4, 0 (a2); \
+        j complience_halt_loop; \
+complience_halt_break:; \
+        lw a4, 0 (a2); \
+        bne x0, a4, complience_halt_break;\
+        li a4, 0xFF; \
+        sw a4, 0 (a2); \
+        j complience_halt_break; \
+
+
+#define RV_COMPLIANCE_RV32M                                                   \
+
+//Nops are for RVC JAL test which require offset
+#ifdef RVC
+#define RV_COMPLIANCE_CODE_BEGIN                                              \
+        .section .text.init;                                                  \
+        .align  4;                                                            \
+        .globl _start;                                                        \
+        _start:                                                               \
+        nop;nop;nop;nop;nop;nop;nop;nop; \
+        nop;nop;nop;nop;nop;nop;nop;nop; \
+        nop;nop;nop;nop;nop;nop;nop;nop; \
+        nop;nop;nop;nop;nop;nop;nop;nop; \
+        nop;nop;nop;nop;nop;nop;nop;nop; \
+        nop;nop;nop;nop;nop;nop;nop;nop; \
+        nop;nop;nop;nop;nop;nop;nop;nop; \
+        nop;nop;nop;nop;nop;nop;nop;nop; \
+        nop;nop;nop;nop;nop;nop;nop;nop; \
+        nop;nop;nop;nop;nop;nop;nop;nop; \
+        nop;nop;nop;nop;nop;nop;nop;nop; \
+        nop;nop;nop;nop;nop;nop;nop;nop; \
+        nop;nop;nop;nop;nop;nop;nop;nop; \
+        nop;nop;nop;nop;nop;nop;nop;nop; \
+        nop;nop;nop;nop;nop;nop;nop; 
+#else
+#define RV_COMPLIANCE_CODE_BEGIN                                              \
+        .section .text.init;                                                  \
+        .align  4;                                                            \
+        .globl _start;                                                        \
+        _start:                                                               
+#endif
+
+
+#define RV_COMPLIANCE_CODE_END                                                \
+
+
+#define RV_COMPLIANCE_DATA_BEGIN                                              \
+        .align 4;                                                             \
+        .global codasip_signature_start;                                      \
+        codasip_signature_start:                                              \
+
+#define RV_COMPLIANCE_DATA_END                                                \
+        .align 4;                                                             \
+        .global codasip_signature_end;                                        \
+        codasip_signature_end:                                                 \
+
+#endif
